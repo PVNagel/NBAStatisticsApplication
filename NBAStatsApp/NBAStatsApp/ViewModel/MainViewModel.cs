ade83aab96380c0bea21c64acc0b3eb917083874
PVNagel <philip.v.nagel@gmail.com>
2023-04-21 16:10:42 +0200

    Added binding for selected player in the list for that players information to be representet.

--- NBAStatsApp/NBAStatsApp/ViewModel/MainViewModel.cs
+++ NBAStatsApp/NBAStatsApp/ViewModel/MainViewModel.cs
@@ -3,6 +3,7 @@ using NBAStatsApp.Model.Repository;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,23 @@ using System.Windows.Input;
 
 namespace NBAStatsApp.ViewModel
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
         private PlayerRepo playerRepo = new PlayerRepo();
 
         public ObservableCollection<PlayerViewModel> PlayersVM { get; set; } = new();
+
         private PlayerViewModel playerViewModel;
+        public PlayerViewModel SelectedPlayerVM
+        {
+            get { return playerViewModel; }
+            set 
+            { 
+                playerViewModel = value; 
+                OnPropertyChanged("SelectedPlayerVM"); 
+            }
+        }
+
         public MainViewModel()
         {
             playerRepo.PlayerAPI();
@@ -23,6 +35,16 @@ namespace NBAStatsApp.ViewModel
             {
                 PlayerViewModel playerVM = new(player);
                 PlayersVM.Add(playerVM);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
+            if (propertyChanged != null)
+            {
+                propertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
         }
     }
